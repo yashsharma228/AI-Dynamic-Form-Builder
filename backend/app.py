534bd68719e219d469579cde59d40e0b4fda1b2f
Yash Sharma <yashsharma228>
2026-03-30 19:03:50 +0530

    saved my changes

--- backend/app.py
+++ backend/app.py
@@ -101,5 +101,6 @@ def register_error_handlers(app: Flask):
 
 if __name__ == "__main__":
 	app = create_app()
-	app.run(host="0.0.0.0", port=5000, debug=False)
+	port = int(os.environ.get("PORT", 5000))
+	app.run(host="0.0.0.0", port=port, debug=False)
 
